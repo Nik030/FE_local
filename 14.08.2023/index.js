a814ad3a4f1f8b7a48c9a483fc8ca994f7d4631b
Nikita-Schneider <nikitasch2001@gmail.com>
2023-08-14 09:51:00 +0200

    .

---
+++ 14.08.2023/index.js
@@ -0,0 +1,22 @@
+const transactions = [
+    {
+        from: "Jhon",
+        amount: 3500,
+        currency: "USD",
+        to: "Lu Kan"
+    },
+
+    {
+        from: "Ember",
+        amount: 2140,
+        currency: "EUR",
+        to: "Jerar"
+    },
+
+    {
+        from: "Vazgen",
+        amount: 30000,
+        currency: "RUB",
+        to: "Emil"
+    }
+]
